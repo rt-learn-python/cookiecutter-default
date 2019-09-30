68f745b8913fc2659fff7d00fda0ab7ccc235d7a
Royce Remulla <royce.com@gmail.com>
2019-09-30 14:18:48 +0800

    Fix escaping mustaches

--- {{cookiecutter.project_slug}}/logger.py
+++ {{cookiecutter.project_slug}}/logger.py
@@ -6,10 +6,11 @@ instance.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
+{% raw %}
 formatter = logging.Formatter(
     '%(asctime)s - %(levelname)s {%(pathname)s:%(lineno)d} - %(message)s',
     '%Y-%m-%d %H:%M:%S')
-
+{% endraw %}
 
 # add formatter to ch
 ch.setFormatter(formatter)
